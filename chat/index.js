368d5901e615821336cd50a1a24a2efdfeaee0e7
Dante Santoso <35228158+D-X-V-Santoso@users.noreply.github.com>
2020-03-25 20:55:42 +0100

    Added user dis/connected message

--- chat/index.js
+++ chat/index.js
@@ -12,6 +12,28 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('chat message','A user connected')
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    io.emit('chat message','A user disconnected')
+  });
+});
+
+/*
+io.on('connection', function (socket) {
+  socket.on('disconnect', function(){
+  io.emit('user connected');
+  });
+});
+
+io.on('disconnect', function (socket) {
+  socket.on('disconnect', function(){
+  io.emit('user disconnected');
+  });
+});*/
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
