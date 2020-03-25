4da648d1ac650d1d7ad3c9128abf5518aa3ede21
Aine Dwane <aineishuman@gmail.com>
2020-03-25 18:11:22 +0100

    Edn of tutorial

--- chat/index.js
+++ chat/index.js
@@ -7,13 +7,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-    });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
