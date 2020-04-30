0876f0b0e5cec267a1509c5673b258aa4a7361a2
Hwajun1323 <whajun1323@gmail.com>
2020-04-30 21:23:24 +0200

    package.json, modified page/login.html, and server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ if (process.env.NODE_ENV !== 'production') {
   const flash = require('express-flash')
   const session = require('express-session')
   const methodOverride = require('method-override')
+  const morgan = require('morgan')
+  const bodyParser = require('body-parser')
   const mongoose = require('mongoose')
   mongoose.connect('mongodb://134.122.56.116')
   
@@ -20,6 +22,15 @@ if (process.env.NODE_ENV !== 'production') {
   )
   
   const users = []
+  const db = mongoose.connection
+
+  db.on('error',(err)=>{
+    console.log(err)
+  })
+
+  db.once('open',() => {
+    console.log('Database Connection Established!')
+  })
   
   app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
@@ -33,7 +44,15 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(passport.session())
   app.use(methodOverride('_method'))
   app.use(express.static(__dirname + '/public'));
+  app.use(morgan('dev'))
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json())
 
+  const PORT = process.env.PORT || 3000
+
+  app.listen(PORT,()=>{
+    console.log('Server is running on port ${PORT}')
+  })
   
   app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', { name: req.user.name })
