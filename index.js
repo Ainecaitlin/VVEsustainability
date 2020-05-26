240a1bc45ee708e1ac5f68e490015403e956b481
Hwajun1323 <whajun1323@gmail.com>
2020-05-26 02:12:39 +0200

    front-end
    there are few things to fix

--- index.js
+++ index.js
@@ -7,19 +7,7 @@ var session = require('express-session');
 var passport = require('./config/passport');
 var util = require('./util');
 var app = express();
-//==== Adding some debugging stuff here ====///
-  var http=require('http');
-
-var server=http.createServer(function(req,res){
-    res.end('test');
-});
-
-server.on('listening',function(){
-    console.log('ok, server is running');
-});
-
-server.listen(8000);
-//========== End of debug stuff =======///
+  
 // DB setting
 const db = require('./config/key').MongoURI;
 
@@ -30,7 +18,6 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 
 //other setting  
 app.set('view engine', 'ejs');
-app.set('views', './views');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -58,7 +45,7 @@ app.use('/comments', util.getPostQueryString, require('./routes/comments'));
 app.use('/files', require('./routes/files'));
   
 // Port setting
-var port = 8000;
+var port = process.env.PORT || 4000;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
--- routes/home.js
+++ routes/home.js
@@ -9,6 +9,77 @@ router.get('/', function(req, res){
 router.get('/about', function(req, res){
   res.render('home/about');
 });
+// advice
+router.get('/advice', function(req, res){
+  res.render('advice/advice');
+});
+router.get('/advice-information', function(req, res){
+  res.render('advice/information');
+});
+router.get('/advice-search', function(req, res){
+  res.render('posts');
+});
+// led lighting
+router.get('/led', function(req, res){
+  res.render('led/led');
+});
+router.get('/led-information', function(req, res){
+  res.render('led/information');
+});
+router.get('/led-search', function(req, res){
+  res.render('posts');
+});
+// insulation
+router.get('/insulation', function(req, res){
+  res.render('insulation/insulation');
+});
+router.get('/insulation-information', function(req, res){
+  res.render('insulation/information');
+});
+router.get('/insulation-search', function(req, res){
+  res.render('posts');
+});
+// hr++
+router.get('/hr', function(req, res){
+  res.render('hr/hr');
+});
+router.get('/hr-information', function(req, res){
+  res.render('hr/information');
+});
+router.get('/hr-search', function(req, res){
+  res.render('posts');
+});
+// energy generation
+router.get('/energy-generation', function(req, res){
+  res.render('energy-generation/energy-generation');
+});
+router.get('/energy-generation-information', function(req, res){
+  res.render('energy-generation/information');
+});
+router.get('/energy-generation-search', function(req, res){
+  res.render('posts');
+});
+// charging station
+router.get('/charging-station', function(req, res){
+  res.render('charging-station/chargin-station');
+});
+router.get('/charging-station-information', function(req, res){
+  res.render('charging-station/information');
+});
+router.get('/charging-station-search', function(req, res){
+  res.render('posts');
+});
+// finance
+router.get('/finance', function(req, res){
+  res.render('finance/finanace');
+});
+router.get('/finanace-information', function(req, res){
+  res.render('finance/information');
+});
+router.get('/finanace-search', function(req, res){
+  res.render('posts');
+});
+
 
 // Login
 router.get('/login', function (req,res) {
