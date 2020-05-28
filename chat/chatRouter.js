495fa1e7c18ce0889067492735eaa18a917bd64e
rayyan808 <49687695+rayyan808@users.noreply.github.com>
2020-05-28 03:02:38 +0200

    Changed route location
    To avoid glitching the main page whilst debugging the code

--- chat/chatRouter.js
+++ chat/chatRouter.js
@@ -4,7 +4,7 @@ const Chat = require("./chatschema.js");
 
 const  router  =  express.Router();
 
-router.route("/").get((req, res, next) =>  {
+router.route("/chat").get((req, res, next) =>  {
         res.setHeader("Content-Type", "application/json");
         res.statusCode  =  200;
         connect.then(db  =>  {
