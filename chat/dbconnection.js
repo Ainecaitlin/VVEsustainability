bc341896cecd326a5700a9f599e48b4b67f06a6f
rayyan808 <49687695+rayyan808@users.noreply.github.com>
2020-05-28 03:05:45 +0200

    Server IP setting

--- chat/dbconnection.js
+++ chat/dbconnection.js
@@ -1,6 +1,6 @@
 
 const  mongoose  = require("mongoose");
 mongoose.Promise  = require("bluebird");
-const  uri  =  "mongodb+srv://tuhanyesus:ampunilahdosakami@chat-test-4vpc2.mongodb.net/test?retryWrites=true&w=majority";
+const  uri  =  "mongodb+srv://localhost:27071/VVE-DB";
 const  connect  =  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports  =  connect;
