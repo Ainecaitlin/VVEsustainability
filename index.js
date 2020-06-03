c942327d5b67129d9a5ae251286645611ca138af
rayyan808 <49687695+rayyan808@users.noreply.github.com>
2020-06-03 19:21:50 +0200

    Commenting my Code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//******** Contributor: Rayyan Jafri *********//
+
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -13,25 +15,14 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 io.set("transports", ["websocket"]);
-/*http.listen(8000);(server, {
-    handlePreflightRequest: (req, res) => {
-        console.log("Setting headers.");
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-});*/
+
 // Port setting
 var port = 8000;
 
 const path = require('path')
 // DB setting
 const db = require('./config/key').MongoURI;
-//Connect Express
+//Catch a connection event that has been routed to the node server
 io.on('connection', socket  =>  {
         console.log("user connected");
         socket.on('disconnect', function() {
@@ -47,20 +38,13 @@ io.on('connection', socket  =>  {
             chatMessage.save();
         })
     });
-//Connect mongo
+//Connect Mongo
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 .then(() => {
     console.log('DB connected...');
-    
+    //Wait for the Promise to be fulfilled
 })
 .catch(err => console.log(err));
-//setup event listener AFTER DB connection fulfills promise
- /*   io.origins((origin, callback) => {
-  if (origin !== 'http://64.227.79.217') {
-    return callback('origin not allowed', false);
-  }
-  callback(null, true);
-});*/
     
 //other setting  
 app.set('view engine', 'ejs');
@@ -97,7 +81,7 @@ app.get('/givemejquery', function(req, res){ //Providing the client with our jqu
     app.get('/givemeasocket', function(req, res){ //Providing the client with our socket.js
         res.sendFile(__dirname + '/node_modules/socket.io/lib/socket.js');
     });
-/* ************** */
+/* *******ACTIVATE SERVER LISTENER******* */
 http.listen(port, function(){
   console.log('server on! http://localhost:'+port + "app.get(port) = " + app.get('port'));
 
