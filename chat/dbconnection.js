a2292aba083d8b6befa14c4af8081fa94963f74f
rayyan808 <rayyan808@gmail.com>
2020-06-11 18:36:00 +0200

    delete garbage

--- chat/dbconnection.js
+++
@@ -1,5 +0,0 @@
-const  mongoose  = require("mongoose");
-mongoose.Promise  = require("bluebird");
-const  url  =  "mongodb://127.0.0.1";
-const  connect  =  mongoose.connect(url, { useNewUrlParser: true  });
-module.exports  =  connect;
