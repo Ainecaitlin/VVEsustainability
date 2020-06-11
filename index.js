fc6471a7fb8ab138db3ce2d3432c3ea180beb3ed
Dante Santoso <35228158+D-X-V-Santoso@users.noreply.github.com>
2020-06-11 21:16:23 +0200

    fixed path error and chat room navigation. BUG = chat messages cannot be saved due to typeError.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var express = require('express');
 
 //require the http module
 // require the socket.io module & express
-var Chat = require('./routes/chatschema.js')
+var Chat= require('./routes/chatschema.js')
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
@@ -33,7 +33,7 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 //other setting  
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
@@ -65,13 +65,14 @@ app.use('/chats/:rooms', require('./routes/home'));
 //Create an Array of NameSpaces, For each run the code below
 var nameSpaces = [];
 var rooms = ['General-Chat','Solar-Panel','Finance', 'Charging-Station' ];
-/*var fs = require('fs');
+
+var fs = require('fs');
 var array = fs.readFileSync('./config/chatroomMasterFile.txt').toString().split("\n");
 for(i in array) {
     console.log("adding room:" + array[i]);
     rooms.push(array[i]);
     console.log("rooms["+ i + "]: " + rooms[rooms.length-1]);
-} *///= 
+} //= 
 console.log("Pulling Chatroom Master file....:" + rooms[rooms.length-1]);
 var CHAT_ROOMS = 7; //The number of chatrooms, this variable controls the ini of sockets, namespaces and routes.
 for(var room in rooms){
@@ -87,10 +88,10 @@ for(var room in rooms){
             socket.on('chat message', function(data) {
                 console.log("message: "  +  data.message + "User: " + data.sender + "Room:" + rooms[i]);
                 //broadcast message from client A to all clients in client A's current room
-                nameSpaces[i].to(room).emit("received", { message: data.message, sender: data.sender  });
+                nameSpaces[i].to(room).emit("received", { message: data.message, sender: data.sender ,chatroom:rooms[i] });
                 
             //I am testing if the message functionality stores this message or not
-            let  chatMessage  =  new Chat({ message: data.message, sender: data.sender, chatroom: rooms[i] });
+            let  chatMessage  =  new Chat({ sender: data.sender, chatroom: rooms[i] ,message: data.message});
             chatMessage.save();
         })
     });
